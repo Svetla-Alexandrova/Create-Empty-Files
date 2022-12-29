6258b0c50eb4b17e65ab4d9c41948f895752e396
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-29 14:08:50 +0000

    Create Archive from zipFiles

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from pathlib import Path
+import zipfile
+
+# root_dir = Path('files')
+
+# for i in range(1, 11):
+#   filename = str(i) + ".txt"
+#   filepath = root_dir / Path(filename)
+#   filepath.touch()
 
 root_dir = Path('files')
+archive_path = root_dir / Path("archive.zip")
 
-for i in range(1, 11):
-  filename = str(i) + ".txt"
-  filepath = root_dir / Path(filename)
-  filepath.touch()
+with zipfile.ZipFile("archive_path", "w") as zf:
+  for path in root_dir.rglob("*.txt"):
+    zf.write(path)
   
